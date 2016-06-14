983db3f762b3de97dc944056ce6d1f715b4d6e75
MRCai001002003 <294042223@qq.com>
2016-06-14 19:12:21 +0800

    fdsa

--- src/main/webapp/resources/src/pages/registerInfo/registerInfo.js
+++ src/main/webapp/resources/src/pages/registerInfo/registerInfo.js
@@ -1,22 +1,29 @@
 define(function(require) {
-	var baseUrl=require('../../common/js/baseUrl/baseUrl');
+    var baseUrl = require('../../common/js/baseUrl/baseUrl');
     return function(app) {
-        app.controller('registerInfoControl', ['$scope', '$http','$state',
-            function($scope, $http,$state) {
-				$scope.params={};
-				$scope.submit=function(){
-					$http({
-						url:baseUrl+'customer/saveOrUpdateCustomerPersonal',
-						method:'post',
-						data:$scope.params
-					}).success(function(data){
-						if(data.success){
-							$state.go('registed');
-						}else{
-							alert(data.msg);
-						}
-					})	
-				}
+        app.controller('registerInfoControl', ['$scope', '$http', '$state',
+            function($scope, $http, $state) {
+                $scope.params = {};
+                $scope.isShowSuffix = false;
+                $scope.testSuffix = function(value) {
+                    $scope.isShowSuffix = (value || '').indexOf('@') > -1;
+                }
+                $scope.submit = function() {
+                    if ($scope.isShowSuffix) {
+                        $scope.params.email += $scope.emailSuffix;
+                    }
+                    $http({
+                        url: baseUrl + 'customer/saveOrUpdateCustomerPersonal',
+                        method: 'post',
+                        data: $scope.params
+                    }).success(function(data) {
+                        if (data.success) {
+                            $state.go('registed');
+                        } else {
+                            alert(data.msg);
+                        }
+                    })
+                }
             }
         ])
     }
