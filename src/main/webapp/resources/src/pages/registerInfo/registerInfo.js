39b2fd418f8cd10762b608c66d03f87fea9260a5
MRCai001002003 <294042223@qq.com>
2016-06-15 10:20:38 +0800

    fdsa

--- src/main/webapp/resources/src/pages/registerInfo/registerInfo.js
+++ src/main/webapp/resources/src/pages/registerInfo/registerInfo.js
@@ -3,15 +3,45 @@ define(function(require) {
     return function(app) {
         app.controller('registerInfoControl', ['$scope', '$http', '$state',
             function($scope, $http, $state) {
+                var emailList = ['@qq.com', '@163.com', '@126.com', '@139.com', '@sina.com', '@hotmail.com', '@aliyun.com', '@sohu.com'];
                 $scope.params = {};
                 $scope.isShowSuffix = false;
+                $scope.emailList = emailList;
+                $scope.isEmail = true;
+
+                $scope.addSuffix = function(text) {
+                    $scope.params.email += text;
+                    $scope.isShowSuffix = false;
+                    $scope.isEmail = true;
+                };
+
                 $scope.testSuffix = function(value) {
-                    $scope.isShowSuffix = (value || '').indexOf('@') > -1;
+                    if (!value) {
+                        $scope.isShowSuffix = false;
+                        return;
+                    }
+                    var index = (value || '').indexOf('@');
+                    if (index == -1) {
+                        $scope.emailList = emailList;
+                        $scope.isShowSuffix = true;
+                    } else {
+                        var arr = [];
+                        var reg = new RegExp('^' + value.substring(index, value.length));
+                        emailList.forEach(function(item) {
+                            if (reg.test(item)) {
+                                arr.push(item.replace(reg, ''));
+                            }
+                        })
+                        $scope.emailList = arr;
+                        $scope.isShowSuffix = !!arr.length;
+                    }
                 }
                 $scope.submit = function() {
-                    if ($scope.isShowSuffix) {
-                        $scope.params.email += $scope.emailSuffix;
+                    if (!/^[^\s@]+@[^\s@]+\.\S+$/.test($scope.params.email)) {
+                        $scope.isEmail = false;
+                        return;
                     }
+
                     $http({
                         url: baseUrl + 'customer/saveOrUpdateCustomerPersonal',
                         method: 'post',
