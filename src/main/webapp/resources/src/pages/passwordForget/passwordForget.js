a5d4e4fa8b1bb2ed66f7c3758f712f5eea9f7e3b
MRCai001002003 <294042223@qq.com>
2016-06-21 17:34:23 +0800

    passwordForget

--- src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
+++ src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
@@ -2,7 +2,14 @@ define(function(require) {
     var baseUrl = require('../../common/js/baseUrl/baseUrl');
     return function(app) {
         app.controller('passwordForgetControl', ['$scope', '$http', function($scope, $http) {
+            $scope.getVerifyCode = function() {
+                $http({
+                    url: baseUrl + 'index/getValidateCode',
+                    method: 'get'
+                }).success(function(data) {
 
+                })
+            }
         }])
     }
 })
