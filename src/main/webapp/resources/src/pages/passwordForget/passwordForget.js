c3b4f20b1b2348325fe437a6138a0915240f4a5e
MRCai001002003 <294042223@qq.com>
2016-06-22 15:43:25 +0800

    fdsa

--- src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
+++ src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
@@ -2,18 +2,20 @@ define(function(require) {
     var baseUrl = require('../../common/js/baseUrl/baseUrl');
     return function(app) {
         app.controller('passwordForgetControl', ['$scope', '$http', 'cache', function($scope, $http, cache) {
+            $scope.params = {
+                cellPhone: cache.get('phone')
+            };
             $scope.getVerifyCode = function() {
-                $scope.params = {
-                    cellPhone: cache.get('phone')
-                };
                 $http({
                     url: baseUrl + 'index/getValidateCode',
-                    params:{cellPhone: scope.params.cellPhone},
-                    method: 'get'
+                    method: 'get',
+                    params: {
+                        cellPhone: $scope.params.cellPhone
+                    }
                 }).success(function(data) {
-                	if(!data.success){
-                		alert(data.msg)
-                	}
+                    if (!data.success) {
+                        alert(data.msg);
+                    }
                 })
             }
             $scope.submit = function() {
@@ -22,9 +24,9 @@ define(function(require) {
                     method: 'post',
                     data: $scope.params
                 }).success(function(data) {
-                	if(!data.success){
-                		alert(data.msg)
-                	}
+                    if (!data.success) {
+                        alert(data.msg);
+                    }
                 })
             }
         }])
