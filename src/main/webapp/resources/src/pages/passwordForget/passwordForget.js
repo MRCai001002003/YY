d3934cf65cf05f0d6715d02e990d389c8f07d1c1
18616392776 <464328895@qq.com>
2016-07-06 18:57:10 +0800

    打包

--- src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
+++ src/main/webapp/resources/src/pages/passwordForget/passwordForget.js
@@ -1,34 +1,8 @@
 define(function(require) {
     var baseUrl = require('../../common/js/baseUrl/baseUrl');
     return function(app) {
-        app.controller('passwordForgetControl', ['$scope', '$http', 'cache', function($scope, $http, cache) {
-            $scope.params = {
-                cellPhone: cache.get('phone')
-            };
-            $scope.getVerifyCode = function() {
-                $http({
-                    url: baseUrl + 'index/getValidateCode',
-                    method: 'get',
-                    params: {
-                        cellPhone: $scope.params.cellPhone
-                    }
-                }).success(function(data) {
-                    if (!data.success) {
-                        alert(data.msg);
-                    }
-                })
-            }
-            $scope.submit = function() {
-                $http({
-                    url: baseUrl + 'index/doSetServerCode',
-                    method: 'post',
-                    data: $scope.params
-                }).success(function(data) {
-                    if (!data.success) {
-                        alert(data.msg);
-                    }
-                })
-            }
+        app.controller('passwordForgetControl', ['$scope', '$http', function($scope, $http) {
+
         }])
     }
 })
