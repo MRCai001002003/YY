9af55354198a834f71fb2b47695d23989a28cb1e
MRCai001002003 <294042223@qq.com>
2016-07-14 10:04:46 +0800

    补充个人中心调用接口

--- src/main/webapp/resources/src/pages/menberCenter/menberCenter.js
+++ src/main/webapp/resources/src/pages/menberCenter/menberCenter.js
@@ -2,7 +2,19 @@ define(function(require) {
     var baseUrl = require('../../common/js/baseUrl/baseUrl');
     return function(app) {
         app.controller('menberCenterControl', ['$scope', '$http', function($scope, $http) {
-
+			$scope.submit=function(){
+				$http({
+					url:baseUrl+'customer/getMenberCenter',
+					method:'get',
+				}).success(function(data){
+					if(data.success){
+						$scope.data=data;
+					}else{
+						alert(data.msg);
+					}
+				})	
+			}
+        
         }])
     }
 })
