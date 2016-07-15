71efd1dad5f94423f074959adcc27bf19da0269a
18616392776 <464328895@qq.com>
2016-07-15 21:53:33 +0800

    fdsa

--- src/main/webapp/resources/requirejs-config.js
+++ src/main/webapp/resources/requirejs-config.js
@@ -1,5 +1,5 @@
 require.config({
-    baseUrl: './resources/src/',
+    baseUrl: './resources/dist/',
     paths: {
         'angular': '../library/angular/angular.min',
         'ui.router': '../library/angular-ui-router/release/angular-ui-router.min',
