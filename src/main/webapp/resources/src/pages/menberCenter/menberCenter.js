a5a51984cf3654e6249a1af22408473c2d6a01fc
18616392776 <464328895@qq.com>
2016-07-19 20:07:40 +0800

    getTaoBaoInfo

--- src/main/webapp/resources/src/pages/menberCenter/menberCenter.js
+++ src/main/webapp/resources/src/pages/menberCenter/menberCenter.js
@@ -1,8 +1,20 @@
-define(function (require) {
+define(function(require) {
     var baseUrl = require('../../common/js/baseUrl/baseUrl');
-    return function (app) {
-        app.controller('menberCenterControl', ['$scope', '$http', 'cache', function ($scope, $http, cache) {
+    return function(app) {
+        app.controller('menberCenterControl', ['$scope', '$http', 'cache', function($scope, $http, cache) {
             $scope.name = cache.get('name');
+            $scope.getTaoBaoInfo = function() {
+                $http({
+                    url: baseUrl + 'openLoginWin',
+                    method: 'get'
+                }).success(function(data) {
+                    if (data.success) {
+                        location.href = data;
+                    }else{
+                        alert(data.msg);
+                    }
+                })
+            }
         }])
     }
 })
